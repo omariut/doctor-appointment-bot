616a27c78524a04415c5ceca203ac0912c565977
Omar Faruk <84440130+omariut@users.noreply.github.com>
2025-08-27 10:32:17 +0600

    feat : Add response function in agent

--- app/ai_agent/agent.py
+++ app/ai_agent/agent.py
@@ -4,9 +4,13 @@ load_dotenv()  # loads from .env
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage
-
+from .prompts import PROMPT
+from .context import CONTEXT
 
 class AppointmentAgent:
     def __init__(self):
         self.gemini_2_5_flash_llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
     
+    def get_response(self, user_message: str):
+        prompt = PROMPT.format(user_message=user_message, **CONTEXT)
+        return self.gemini_2_5_flash_llm.invoke(prompt).content
