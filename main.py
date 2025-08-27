07a5ba907b9ffc80888915b995a2cfff889b3505
Omar Faruk <84440130+omariut@users.noreply.github.com>
2025-08-27 08:17:14 +0600

    feat : Add chat API with dummy text from bot

--- main.py
+++ main.py
@@ -8,7 +8,17 @@ app = FastAPI()
 def index():
     return open("static/chat.html", "r", encoding="utf-8").read()
 
-
+@app.post("/api/chat", response_class=HTMLResponse)
+async def chat_endpoint( message: str = Form(...)):
+    # Echo the user message
+    user_html = f'<div class="user msg">{message}</div>'
+    
+    # bot logic
+    reply_text = "Hello! Tell me doctor's name and I will help you to book an appointment."
+    bot_html = f'<div class="bot msg">{reply_text}</div>'
+    
+    # Return both messages so they append to chat
+    return HTMLResponse(user_html + bot_html)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
