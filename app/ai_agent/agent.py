7b7e675c15759c16798d22a11d9b08a424df0cd6
Omar Faruk <84440130+omariut@users.noreply.github.com>
2025-08-29 15:09:58 +0600

    feat (agent) : Add langsmith trace and tool in agent

--- app/ai_agent/agent.py
+++ app/ai_agent/agent.py
@@ -5,9 +5,15 @@ load_dotenv()  # loads from .env
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage
-from .prompts import PROMPT
-from .context import CONTEXT
-from .qdrant import QdrantIngestionService
+from app.ai_agent.prompts import PROMPT, chat_prompt
+from app.ai_agent.context import CONTEXT
+from app.ai_agent.qdrant import QdrantIngestionService
+from app.ai_agent.tools import save_appointment
+from langchain_core.output_parsers import PydanticToolsParser
+from langchain_core.messages import HumanMessage, ToolMessage
+from datetime import datetime
+from langchain_core.tracers.context import collect_runs
+from langsmith.run_helpers import traceable
 
 
 class AppointmentAgent:
@@ -15,11 +21,34 @@ class AppointmentAgent:
         self.gemini_2_5_flash_llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
         self.qdrant = QdrantIngestionService()
 
+    @traceable(name="hospital-assistant-session")
     def get_response(self, user_message: str):
         docs = self.get_docs(user_message)
-        context = "\n".join([doc.page_content for doc in docs])
-        prompt = PROMPT.format(user_message=user_message, context=context)
-        return self.gemini_2_5_flash_llm.invoke(prompt).content
+
+        doctors_text = "\n".join([doc.page_content for doc in docs])
+        llm_with_tools = self.gemini_2_5_flash_llm.bind_tools([save_appointment])
+        chain = chat_prompt | llm_with_tools
+        messages = [HumanMessage(content=user_message)]
+
+        response = chain.invoke(
+            {
+                "doctors": doctors_text,
+                "user_message": messages,
+                "today": datetime.now().strftime("%Y-%m-%d"),
+            }
+        )
+        messages.append(response)
+        if response.tool_calls:
+            for tool_call in response.tool_calls:
+                selected_tool = {"save_appointment": save_appointment}[
+                    tool_call["name"].lower()
+                ]
+                tool_msg = selected_tool.invoke(tool_call)
+                messages.append(tool_msg)
+
+            followup = llm_with_tools.invoke(messages)
+            return followup.content
+        return response.content
 
     def get_docs(self, user_message: str):
         results = self.qdrant.search(user_message)
