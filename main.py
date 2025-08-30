4568537b719ba22d268f0e790d37ac87926fde7e
Omar Faruk <84440130+omariut@users.noreply.github.com>
2025-08-30 18:19:05 +0600

    feat : Add session id at chat api

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Request, Response, Depends
 from fastapi.responses import HTMLResponse
 import uvicorn
 from app.ai_agent.qdrant import QdrantIngestionService
 from dotenv import load_dotenv
 from typing import List
 from app.ai_agent.agent import AppointmentAgent
+import uuid
 
 load_dotenv()  # loads from .env
 from app import app
+
+
+async def get_session_id(request: Request):
+    # check if session already has an id
+    if "session_id" not in request.session:
+        session_id = str(uuid.uuid4())
+        request.session["session_id"] = session_id
+
+    return request.session["session_id"]
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -16,13 +26,14 @@ def index():
 
 
 @app.post("/api/chat", response_class=HTMLResponse)
-async def chat_endpoint(message: str = Form(...)):
-    # Echo the user message
-    user_html = f'<div class="user msg">{message}</div>'
+async def chat_endpoint(
+    request: Request,
+    message: str = Form(...),
+    session_id: str = Depends(get_session_id),
+):
     agent: AppointmentAgent = app.state.appointment_agent
-
     # bot logic
-    reply_text = agent.get_response(message)
+    reply_text = agent.get_response(message, session_id)
     bot_html = f'<div class="bot msg">{reply_text}</div>'
 
     # Return both messages so they append to chat
